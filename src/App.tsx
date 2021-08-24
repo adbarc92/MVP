2c68bfb79aaba958d407d132e2376e52b4b923f5
adbarc92 <adbarclay92@gmail.com>
2021-08-24 14:28:20 -0600

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TipTap from './components/TipTap';
+
+function App() {
+  return (
+    <div className='App'>
+      <TipTap />
+    </div>
+  );
+}
+
+export default App;
