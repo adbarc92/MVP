2b88f4e0afa50c85f7db5ef29ac11d22d8ba4e2a
adbarc92 <adbarclay92@gmail.com>
2021-08-25 09:26:15 -0600

    Partial rebuild

--- client/App.tsx
+++ client/App.tsx
@@ -1,14 +1,2 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import TipTap from './components/TipTap';
-
-function App() {
-  return (
-    <div className='App'>
-      <TipTap />
-    </div>
-  );
-}
-
-export default App;
