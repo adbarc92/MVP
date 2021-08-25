7f8d2de9cf17dac3a6606532776fea83399d83b2
adbarc92 <adbarclay92@gmail.com>
2021-08-24 21:12:51 -0600

    Hmm

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import { Section } from './models/Section';
 import { Section_Node } from './models/Section_Node';
 import { WritingStats } from './models/WritingStats';
 
-import { createConnection } from 'typeorm';
+import { ConnectionManager, createConnection } from 'typeorm';
 import ormconfig from './ormconfig.json';
 
 const app = express();
@@ -18,25 +18,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('common'));
 
-let connection;
+const connectionManager = new ConnectionManager();
+
+const connection = connectionManager.create({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'outliner',
+  entities: [
+    Book,
+    Chapter,
+    Node,
+    Section,
+    Section_Node,
+    WritingStats,
+  ],
+  synchronize: true,
+  logging: false,
+});
 
 const init = async () => {
-  connection = await createConnection({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'outliner',
-    entities: [
-      Book,
-      Chapter,
-      Node,
-      Section,
-      Section_Node,
-      WritingStats,
-    ],
-    synchronize: true,
-    logging: false,
-  });
+  connection
+    .connect()
+    .then((connection) => {
+      console.log('Connected!');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
+
+init();
