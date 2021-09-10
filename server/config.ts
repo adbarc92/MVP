03c1abf163a1dbd8e994296158d9449fe4e9ec2e
adbarc92 <adbarclay92@gmail.com>
2021-09-10 09:29:06 -0600

    Refactors config for env

---
+++ server/config.ts
@@ -0,0 +1,20 @@
+//https://stackoverflow.com/questions/42109813/node-js-environment-variables-and-heroku-deployment
+
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const typeOrmConfig: PostgresConnectionOptions = {
+  name: process.env.DB_CONNECTION_NAME,
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'outlinear',
+  synchronize: true,
+  logging: true
+};
+
+export default typeOrmConfig;
