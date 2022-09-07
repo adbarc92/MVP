6a1aea2782a25fad105a49abfca8e7bba1de176a
adbarc92 <adbarclay92@gmail.com>
2022-09-07 13:36:44 -0600

    Adds default configuration options.

--- server/database/config.ts
+++ server/database/config.ts
@@ -6,9 +6,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const typeOrmConfig: PostgresConnectionOptions = {
-  name: process.env.DB_CONNECTION_NAME,
+  name: process.env.DB_CONNECTION_NAME || 'default',
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
